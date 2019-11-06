8a958ae7070d2dad27bff56e1a262a9dca09594e
snowjak88 <sjnowak88@gmail.com>
2019-11-06 15:43:20 -0500

    Added persistance of RNG state to EngineLoader.

--- assets/data/prefab/mixin/screen-fov-sharing.groovy
+++ assets/data/prefab/mixin/screen-fov-sharing.groovy
@@ -1,10 +1,5 @@
 label = "shares FOV with the screen"
 prefab = {
-	def see = create(CanSee)
-	see.radius = 6
-	
-	create(HasMap)
-	
 	def copiesFov = create(CopiesFOVTo)
 	copiesFov.copyTo = id(tagged(Tags.SCREEN_MAP))
 }
--- core/src/main/java/org/snowjak/hivemind/util/loaders/EngineLoader.java
+++ core/src/main/java/org/snowjak/hivemind/util/loaders/EngineLoader.java
@@ -8,6 +8,7 @@ import java.util.Map.Entry;
 import java.util.logging.Logger;
 
 import org.snowjak.hivemind.Context;
+import org.snowjak.hivemind.RNG;
 import org.snowjak.hivemind.engine.Engine;
 import org.snowjak.hivemind.engine.systems.EntityRefManager;
 
@@ -25,6 +26,13 @@ import squidpony.squidmath.SquidID;
 /**
  * Special-case {@link Loader} to initialize the {@link Engine} currently held
  * in the {@link Context}.
+ * <p>
+ * Note that this will <strong>also</strong> load/unload persisted state for the
+ * following objects:
+ * <ul>
+ * <li>{@link RNG}</li>
+ * </ul>
+ * </p>
  * 
  * @author snowjak88
  *
@@ -40,6 +48,11 @@ public class EngineLoader implements Loader<Engine> {
 			return JsonNull.INSTANCE;
 		
 		final JsonObject obj = new JsonObject();
+		
+		final JsonObject rng = new JsonObject();
+		rng.add("a", new JsonPrimitive(RNG.get().getStateA()));
+		rng.add("b", new JsonPrimitive(RNG.get().getStateB()));
+		obj.add("rng", rng);
 		
 		for (Entity e : src.getEntities()) {
 			
@@ -64,10 +77,28 @@ public class EngineLoader implements Loader<Engine> {
 		
 		final JsonObject obj = json.getAsJsonObject();
 		
-		Context.get();
+		if (obj.has("rng") && obj.get("rng").isJsonObject()) {
+			final JsonObject rng = obj.getAsJsonObject("rng");
+			if (!rng.has("a"))
+				throw new JsonParseException("Cannot parse Engine from JSON -- [rng] missing [a]!");
+			if (!rng.has("b"))
+				throw new JsonParseException("Cannot parse Engine from JSON -- [rng] missing [b]!");
+			try {
+				final int a = Integer.parseInt(rng.get("a").getAsString());
+				final int b = Integer.parseInt(rng.get("b").getAsString());
+				
+				RNG.get().setState(a, b);
+			} catch (NumberFormatException e) {
+				throw new JsonParseException("Cannot parse Engine from JSON -- [rng] is malformed!", e);
+			}
+		}
+		
 		final EntityRefManager erm = Context.getEngine().getSystem(EntityRefManager.class);
 		
 		for (Entry<String, JsonElement> entry : obj.entrySet()) {
+			
+			if (entry.getKey().equalsIgnoreCase("rng"))
+				continue;
 			
 			final SquidID id = context.deserialize(new JsonPrimitive(entry.getKey()), SquidID.class);
 			final Entity e = context.deserialize(entry.getValue(), Entity.class);
